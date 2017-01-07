bd6efb7f7c135dc1b0313610750992daa4c3f893
alex.paclin <alex.paclin@gmail.com>
2017-01-07 21:39:17 +0500

    Работа с ролями в AspNet Identity

--- RolesIdentityApp/Controllers/HomeController.cs
+++ RolesIdentityApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using RolesIdentityApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +11,18 @@ namespace RolesIdentityApp.Controllers
 {
 	public class HomeController : Controller
 	{
+		[Authorize]
 		public ActionResult Index()
 		{
-			return View();
+			IList<string> roles = new List<string> { "Роль не определена" };
+			ApplicationUserManager userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+			ApplicationUser user = userManager.FindByEmail(User.Identity.Name);
+			if (user != null)
+				roles = userManager.GetRoles(user.Id);
+			return View(roles);
 		}
 
+		[Authorize(Roles = "admin")]
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
